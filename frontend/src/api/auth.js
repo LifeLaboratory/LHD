f3355fc62ad858c5862b420bbc93ba1584b452ef
Laloww <lalow.main@gmail.com>
2020-04-18 17:28:18 +0700

    update front..

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+
+export function registerUser (data) {
+  return axios.post(`${process.env.VUE_APP_BACKEND}/api/user/login`, JSON.stringify(data),
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(error)
+      return false
+    })
+}
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,79 @@
+<template>
+<a-row type="flex" justify="center">
+  <a-col :span="12">
+      <a-form
+        id="components-form"
+        :form="form"
+        class="form"
+        @submit="handleSubmit"
+      >
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'login',
+              { rules: [{ required: true, message: 'Введите имя!' }] },
+            ]"
+            placeholder="Username"
+          >
+            <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'password',
+              { rules: [{ required: true, message: 'Введите пароль!' }] },
+            ]"
+            type="password"
+            placeholder="Password"
+          >
+            <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" class="form-button">
+            Зарегестрироваться
+          </a-button>
+          или
+          <a href="">
+            Войти
+          </a>
+        </a-form-item>
+      </a-form>
+    </a-col>
+</a-row>
+</template>
+
+<script>
+import * as authApi from '@/api/auth.js'
+export default {
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal_login' });
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields( async (err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+          let res = await authApi.registerUser(values)
+          if (res !== false) {
+            localStorage.setItem('session', res.session)
+            router.push('/game')
+          } else {
+            this.$message.error('Пользователь с таким именем уже существует');
+          }
+        }
+      });
+    },
+  },
+};
+</script>
+<style>
+#components-form .form {
+  max-width: 300px;
+}
+#components-form .form-button {
+  width: 100%;
+}
+</style>
